47fbf3440f3a5b01a6472401347904ec8b8c8ca1
Luca Bataillard <lucabad@bluewin.ch>
2020-03-26 19:33:37 +0100

    add services

---
+++ scripts/clean_services.py
@@ -0,0 +1,37 @@
+import re
+import csv
+
+from constants import HOME_PATH
+from time import strftime, strptime
+
+FIELDNAMES = ["service_id", "runs_monday", "runs_tuesday", "runs_wednesday", \
+    "runs_thursday", "runs_friday", "runs_saturday", "runs_sunday", "start_date", "end_date"]
+
+def clean(data):
+    return {
+        "service_id": data["service_id"],
+        "runs_monday": data["monday"],
+        "runs_tuesday": data["tuesday"],
+        "runs_wednesday": data["wednesday"],
+        "runs_thursday": data["thursday"],
+        "runs_friday": data["friday"],
+        "runs_saturday": data["saturday"],
+        "runs_sunday": data["sunday"],
+        "start_date": strftime("%Y-%m-%d", strptime(data["start_date"], "%Y%m%d")),
+        "end_date": strftime("%Y-%m-%d", strptime(data["end_date"], "%Y%m%d"))
+    }
+
+with open(HOME_PATH + "/data/calendar.txt", newline="", encoding="utf-8-sig") as csvfile:
+    with open(HOME_PATH + "/data/clean/service.csv", "w", newline="\n") as outputcsv:
+        reader = csv.DictReader(csvfile)
+        writer = csv.DictWriter(outputcsv, fieldnames=FIELDNAMES, quoting=csv.QUOTE_ALL, lineterminator="\n")
+        
+        seen = []
+
+        for row in reader:
+            cleaned = clean(row)
+
+            if cleaned["service_id"] not in seen:
+                writer.writerow(cleaned)
+                seen.append(cleaned["service_id"])
+
--- sql/insert_data.sql
+++ sql/insert_data.sql
@@ -12,5 +12,10 @@ ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-
+LOAD DATA LOCAL INFILE "/home/luca/Coding/CPSC304/railwayplanner/data/clean/service.csv"
+INTO TABLE Service
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"' 
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
 
